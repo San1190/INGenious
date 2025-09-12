8c1cd475f8d49b1d166a7d1e6e96ed24f1779614
ferpasri <ferpasri@gmail.com>
2025-09-12 14:26:10 +0200

    Indicate the agent bdd steps are not modifiable

--- IDE/src/main/java/com/ing/ide/main/testar/mcp/McpInterface.java
+++ IDE/src/main/java/com/ing/ide/main/testar/mcp/McpInterface.java
@@ -20,8 +20,13 @@ import com.ing.ide.main.testar.mcp.helper.McpToolExecutor;
  */
 public interface McpInterface {
 
+    String bddStepDescription = "The BDD step text associated with this action. Do not modify.";
+
     @McpMethod(description = "Load the web URL to be tested.")
-    String loadWebURL(@McpParam(name = "url", description = "The web url to be tested.") String url);
+    String loadWebURL(
+            @McpParam(name = "bddStep", description = bddStepDescription) String bddStep,
+            @McpParam(name = "url", description = "The web url to be tested.") String url
+    );
 
     @McpMethod(description = "Get the current web url that is being tested.")
     String getCurrentURL();
@@ -34,20 +39,20 @@ public interface McpInterface {
 
     @McpMethod(description = "Use a CSS selector to click an element.")
     String executeClickAction(
-            @McpParam(name = "bddStep", description = "The BDD step text associated with this action.") String bddStep,
+            @McpParam(name = "bddStep", description = bddStepDescription) String bddStep,
             @McpParam(name = "cssSelector", description = "The CSS selector of the clickable element.") String cssSelector
     );
 
     @McpMethod(description = "Use a CSS selector to fill text into an input element.")
     String executeFillAction(
-            @McpParam(name = "bddStep", description = "The BDD step text associated with this action.") String bddStep,
+            @McpParam(name = "bddStep", description = bddStepDescription) String bddStep,
             @McpParam(name = "cssSelector", description = "The CSS selector of the input element to be filled.") String cssSelector,
             @McpParam(name = "fillText", description = "The text to be entered into the input element.") String fillText
     );
 
     @McpMethod(description = "Use a CSS selector to select an option value into a select element.")
     String executeSelectAction(
-            @McpParam(name = "bddStep", description = "The BDD step text associated with this action.") String bddStep,
+            @McpParam(name = "bddStep", description = bddStepDescription) String bddStep,
             @McpParam(name = "cssSelector", description = "The CSS selector of the select element to be changed.") String cssSelector,
             @McpParam(name = "optionValue", description = "The option value to be selected into the select element.") String optionValue
     );
@@ -60,7 +65,7 @@ public interface McpInterface {
 
     @McpMethod(description = "Add a unique text that can be used to assert a BDD step.")
     String addStepAssert(
-            @McpParam(name = "bddStep", description = "The BDD step text associated with this action.") String bddStep,
+            @McpParam(name = "bddStep", description = bddStepDescription) String bddStep,
             @McpParam(name = "assertText", description = "A visible unique text that can be used as text locator assert.") String assertText
     );
 
